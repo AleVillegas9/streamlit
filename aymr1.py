a9d0f22378de5e648498c8c1a8c150ccf2727e44
Alejandro Villegas <jesus.villegas@alumnos.cide.edu>
2024-06-30 00:52:23 -0600

    1er borrador esqueleto

---
+++ aymr1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 30 00:45:37 2024
+
+@author: Abraxalandro
+"""
+
+#Nota Este es el esqueleto de la pagina de streamlit. se ira actualizando poco a poco. 
+
+#Paso 1: Cargamos la libreria
+
+import streamlit as st
+
+st.title ("Esqueleto proyecto judicatura")
+
